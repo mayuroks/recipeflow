12f60408ccae36143433cd8cb852270c23c42111
Mayur Rokade <mayuroks@users.noreply.github.com>
2025-05-31 18:24:36 +0530

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css'; // Import your Tailwind CSS here
+import App from './App'; // Import your App component
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
